7d63189140c83a29cf71968c27bdee855ab48050
gunessumre <73123396+gunessumre@users.noreply.github.com>
2024-11-08 13:55:39 +0300

    commit edildi

---
+++ pratikList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        
+        List<string> ziyaretciler = new List<string>
+        {
+            "martin eden",
+            "raskolnikov",
+            "BAL",
+            "Duru",
+            "Sakatto",
+            "Albay Aureliaona"
+        };
+
+        
+        Console.WriteLine("Ziyaretçi Listesi:");
+        foreach (string ziyaretci in ziyaretciler)
+        {
+            Console.WriteLine(ziyaretci);
+        }
+    }
+}
